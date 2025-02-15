674680f134c0293174299e38879bb4df74281d58
khubaiburrehman0748 <khubaiburrehman993@gmail.com>
2025-02-15 05:31:50 -0800

    Update Task 02.cpp

--- LAB 04/Task 02.cpp
+++ LAB 04/Task 02.cpp
@@ -1,1 +1,81 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Car{
+
+    public :
+        string brand;
+        string model;
+        double price;
+        bool availability;
+    
+    Car(){
+        brand="Unknown";
+        model="generic";
+        price=0.0;
+        availability=true;
+    }
+
+    Car(string brand,string model,double price){
+            this->brand=brand;
+            this->model=model;
+            this->price=price;
+            availability=true;
+        }
+
+    void update(string newbrand,string newmodel,double newprice){
+            this->brand=newprice;
+            this->model=newmodel;
+            this->price=newprice;
+        }
+
+    void rentalrequest(){
+        if(availability){
+            availability=false;
+            cout<<brand<<" "<<model<< " has been rented succesfully "<<endl;
+        }
+        else {
+            cout<<brand<<" "<<model<< " has already rented out "<<endl;
+        }
+    }
+
+    void checkavailability(){
+        if(availability){
+            cout<<brand<<" "<<model<< " is available for rent "<<endl;
+        }
+        else{
+            cout<<brand<<" "<<model<< " is not available for rent "<<endl;
+        }
+    }
+
+    void discount(int days){
+        if(days>5 && days<11){
+            price=price*0.95;
+            cout<<"5%"<<"discount is applied ."<<price<<" is the new rental price.";
+        }
+        else if(days>10){
+            price=price*0.90;
+            cout<<"10%"<<"discount is applied ."<<price<<" is the new rental price.";
+        }
+        else{
+            cout<<"discount is not applicable ";
+        }
+    }
+
+    
+};
+
+int main(){
+    Car car1; 
+    car1.checkavailability();
+
+    Car car2("Toyota", "Corolla", 50.0);
+    car2.checkavailability();
+    car2.rentalrequest();
+    car2.checkavailability(); 
+
+    return 0;
+}
 
