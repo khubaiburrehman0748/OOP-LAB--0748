159a04c42ca7fb3d3da557d94d2a042bad1deb38
khubaiburrehman0748 <khubaiburrehman993@gmail.com>
2025-03-08 00:59:05 +0500

    Update Task 01.cpp

--- LAB 06/Task 01.cpp
+++ LAB 06/Task 01.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Employee {
+    public:
+        string name;
+        float salary = 0.0;
+        
+    Employee(string n, float s){
+        name = n;
+        salary = s;
+    }
+
+    void displayDetails(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Salary : $"<<salary<<endl;
+    }
+
+}; 
+
+class Manager : public Employee {
+    public :
+    float bonus = 0.0;
+
+    Manager(string n, float s,float b): Employee(n, s){
+        bonus = b;
+    }
+
+    void displayDetails(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Salary : $"<<salary<<endl;
+        cout<<"bonus : $"<<bonus<<endl;
+    }
+};
+
+
+int main(){
+    string name;
+    float salary, bonus;
+
+    cout << "Enter Manager's Name: ";
+    getline(cin, name);
+
+    cout << "Enter Salary: ";
+    cin >> salary;
+
+    cout << "Enter Bonus: ";
+    cin >> bonus;
+
+    Manager mgr(name, salary, bonus);
+
+    cout << "\nManager Details:\n";
+    mgr.displayDetails();
+}
