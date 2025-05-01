8932a2c29087d2de5d28dc6c82b09516dd7492bc
khubaiburrehman0748 <khubaiburrehman993@gmail.com>
2025-05-01 23:01:10 +0500

    Create Q2.cpp

---
+++ LAB 10/Q2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ifstream in("vehicles.txt");
+    string line;
+    
+    if (!in.is_open()) {
+        cout << "Error opening file" << endl;
+        return 1;
+    }
+
+    getline(in, line); 
+    getline(in, line); 
+    
+    while (getline(in, line)) {
+        if (line.empty()) continue;
+        
+        istringstream iss(line);
+        string type, id, name, year, extraData, certification;
+        
+        if (getline(iss, type, ',') &&
+            getline(iss, id, ',') &&
+            getline(iss, name, ',') &&
+            getline(iss, year, ',') &&
+            getline(iss, extraData, ',') &&
+            getline(iss, certification)) {
+            
+            cout << "Type: " << type << "\n";
+            cout << "ID: " << id << "\n";
+            cout << "Name: " << name << "\n";
+            cout << "Year: " << year << "\n";
+            cout << "ExtraData: " << extraData << "\n";
+            cout << "Certification: " << certification << "\n";
+            cout << "----------------------\n";
+        }
+    }
+    
+    in.close();
+    return 0;
+}
